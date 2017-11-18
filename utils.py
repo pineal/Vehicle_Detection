16b28271411752c8328e4e20ae4fc1ba402c9799
hesen <hesen.zhang@gmail.com>
2017-11-18 01:45:07 -0500

    Workable v.1

---
+++ utils.py
@@ -0,0 +1,115 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+image = mpimg.imread('./test_images/test1.jpg')
+
+# Define a function that takes an image, a list of bounding boxes, 
+# and optional color tuple and line thickness as inputs
+# then draws boxes in that color on the output
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # make a copy of the image
+    draw_img = np.copy(img)
+    for box in bboxes:
+        cv2.rectangle(draw_img, box[0], box[1], color, thick)
+    # draw each bounding box on your image copy using cv2.rectangle()
+    # return the image copy with boxes drawn
+    return draw_img # Change this line to return image copy with boxes
+# Add bounding boxes in this format, these are just example coordinates.
+bboxes = [((600, 100), (200, 200)), ((300, 300), (400, 400))]
+
+result = draw_boxes(image, bboxes)
+plt.imshow(result)
+
+# Define a function to search for template matches
+# and return a list of bounding boxes
+def find_matches(img, template_list):
+    # Define an empty list to take bbox coords
+    bbox_list = []
+    # Define matching method
+    # Other options include: cv2.TM_CCORR_NORMED', 'cv2.TM_CCOEFF', 'cv2.TM_CCORR',
+    #         'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED'
+    method = cv2.TM_CCOEFF_NORMED
+    # Iterate through template list
+    for temp in template_list:
+        # Read in templates one by one
+        tmp = mpimg.imread(temp)
+        # Use cv2.matchTemplate() to search the image
+        result = cv2.matchTemplate(img, tmp, method)
+        # Use cv2.minMaxLoc() to extract the location of the best match
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+        # Determine a bounding box for the match
+        w, h = (tmp.shape[1], tmp.shape[0])
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        # Append bbox position to list
+        bbox_list.append((top_left, bottom_right))
+        # Return the list of bounding boxes
+
+    return bbox_list
+
+#usage of find_matches function
+#bboxes = find_matches(image, templist)
+#result = draw_boxes(image, bboxes)
+#plt.imshow(result)
+
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+# KEEP IN MIND IF YOU DECIDE TO USE THIS FUNCTION LATER
+# IN YOUR PROJECT THAT IF YOU READ THE IMAGE WITH
+# cv2.imread() INSTEAD YOU START WITH BGR COLOR!
+# Define a function to compute color histogram features
+# Pass the color_space flag as 3-letter all caps string
+# like 'HSV' or 'LUV' etc.
+def bin_spatial(img, color_space='RGB', size=(32, 32)):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    else: feature_image = np.copy(img)
+    # Use cv2.resize().ravel() to create the feature vector
+    features = cv2.resize(feature_image, size).ravel()
+    # Return the feature vector
+    return features
+#feature_vec = bin_spatial(image, color_space='RGB', size=(32, 32))
+
+
+# Define a function to return some characteristics of the dataset
+def data_look(car_list, notcar_list):
+    data_dict = {}
+    # Define a key in data_dict "n_cars" and store the number of car images
+    data_dict["n_cars"] = len(car_list)
+    # Define a key "n_notcars" and store the number of notcar images
+    data_dict["n_notcars"] = len(notcar_list)
+    # Read in a test image, either car or notcar
+    example_img = mpimg.imread(car_list[0])
+    # Define a key "image_shape" and store the test image shape 3-tuple
+    data_dict["image_shape"] = example_img.shape
+    # Define a key "data_type" and store the data type of the test image.
+    data_dict["data_type"] = example_img.dtype
+    # Return data_dict
+    return data_dict
+
+#data_info = data_look(cars, notcars)
+
+# Just for fun choose random car / not-car indices and plot example images
+#car_ind = np.random.randint(0, len(cars))
+#notcar_ind = np.random.randint(0, len(notcars))
+
+# Read in car / not-car images
+#car_image = mpimg.imread(cars[car_ind])
+#notcar_image = mpimg.imread(notcars[notcar_ind])
